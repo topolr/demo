c5d6d629eb36206608b325f1256621d243833678
wangjinliang <wangjinliang@huobi.com>
2019-01-11 18:11:09 +0800

    pass

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 let express = require("express");
+let {DistSteamRenderer} = require("adajs/server");
+let Path = require("path");
+
+const distPath = Path.resolve(__dirname, "./dist");
 
 let app = new express();
+
+app.get("/", (req, res) => {
+	let renderer = new DistSteamRenderer({
+		origin: "http://localhost:8080",
+		distPath
+	});
+	renderer.outputStream("/").pipe(res);
+});
 
 module.exports = app;
